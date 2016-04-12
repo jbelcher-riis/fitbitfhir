db59ccc69bcf7539f12a7d99584f7b53e52ec7c4
Jim Belcher <jbelcher@riis.com>
2016-04-12 15:42:55 -0400

    new auth code

--- constants.php
+++ constants.php
@@ -1,4 +1,4 @@
 <?php
 define("BASE_URL","https://fhir-open-api-dstu2.smarthealthit.org/");
-define("AUTH_TOKEN","c254d6cb15b2486e11371feb0c66fcc39b3b3303");
+define("AUTH_TOKEN","0a75e83f2b2982be9013266c1ae37c78532c2d43");
 define("ACCESS_TOKEN_ENDPOINT","https://api.fitbit.com/oauth2/token");
