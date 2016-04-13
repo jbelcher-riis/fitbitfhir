4ea1236493565a933f30ae6a1df4575c8b2c42f1
Jim Belcher <jbelcher@riis.com>
2016-04-13 12:19:27 -0400

    test

--- FitBit/TokenManager.php
+++ FitBit/TokenManager.php
@@ -44,7 +44,7 @@ class TokenManager {
     }
 
     function reauthorize() {
-        header("Location: https://www.fitbit.com/oauth2/authorize?response_type=code&client_id=".$this->userId."&redirect_uri=http%3A%2F%2Ffitbitfhir.riis.com%2Ffitbitfhir%2FdataTransfer.php&scope=activity");
+        header("Location: https://www.fitbit.com/oauth2/authorize?response_type=code&client_id=227PMB&redirect_uri=http%3A%2F%2Ffitbitfhir.riis.com%2Ffitbitfhir%2FdataTransfer.php&scope=activity");
         exit;
     }
     
