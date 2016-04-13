bcb1b69dad6cba8d7d1a19e8ae6f8564e84b6707
Jim Belcher <jbelcher@riis.com>
2016-04-13 12:06:32 -0400

    test

--- FitBit/TokenManager.php
+++ FitBit/TokenManager.php
@@ -10,15 +10,6 @@ class TokenManager {
     private $accessToken;
     private $refreshToken;
     private $userId;
-    private $basicAuth;
-    
-    function getBasicAuth() {
-        return $this->basicAuth;
-    }
-
-    function setBasicAuth($basicAuth) {
-        $this->basicAuth = $basicAuth;
-    }
 
     function getAuthToken() {
         return $this->authToken;
@@ -55,34 +46,31 @@ class TokenManager {
     function reauthorize() {
         header("Location: https://www.fitbit.com/oauth2/authorize?response_type=code&client_id=".$this->userId."&redirect_uri=http%3A%2F%2Ffitbitfhir.riis.com%2Ffitbitfhir%2FdataTransfer.php&scope=activity");
         exit;
-        /*
-        //$url = ACCESS_TOKEN_ENDPOINT."?client_id=".$this->getUserId()."&grant_type=authorization_code&redirect_uri=http%3A%2F%2Ffitbitfhir.riis.com%2Ffitbitfhir%2FdataTransfer.php&code=".$this->authToken;
-        $redirectURI = "redirect_uri=http%3A%2F%2Ffitbitfhir.riis.com%2Ffitbitfhir%2FdataTransfer.php";
-        $url = "https://www.fitbit.com/oauth2/authorize?response_type=code&client_id=".$this->userId."&scope=activity";
+    }
+    
+    function refreshToken() {
+        $url = "https://api.fitbit.com/oauth2/token?grant_type=refresh_token&refresh_token=".$this->refreshToken;
         $ch = curl_init($url);
        
         # Setting our options
         curl_setopt($ch, CURLOPT_HEADER, false);
         curl_setopt($ch, CURLOPT_POST, 1);
-        
         curl_setopt($ch, CURLOPT_HTTPHEADER,
-         
                 array("Content-type: application/x-www-form-urlencoded","Accept: application/json", 
-                    "Authorization: Basic ".$this->basicAuth
+                    "Authorization: Basic MjI3UE1COjcwNGNjZmZiYzgwYWJiMzc3NDgzNmVkMTdlNzljOGIx"
                     )
                 );
-        
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         # Get the response
         $response = curl_exec($ch);
         curl_close($ch);
 
-        var_dump($response);
-        */
-    }
-    
-    function refreshToken() {
+        $objResponse = json_decode($response);
         
+        $this->accessToken = $objResponse->access_token;
+        $this->refreshToken = $objResponse->refresh_token;
+        
+        return $response;
     }
     
     function requestAccessToken() {
--- dataTransfer.php
+++ dataTransfer.php
@@ -7,6 +7,7 @@ require_once("FitBit/TokenManager.php");
 
 $tokenManager = new TokenManager();
 $tokenManager->setUserId("227PMB");
+$tokenManager->setRefreshToken("7e195e38e496d6b280e1507aace40d487ee75dc74f376b3448707b1ae5de9426");
 
 $fitbit = new FitBit($tokenManager);
 $fitbit->getActivity("2016-04-13");
