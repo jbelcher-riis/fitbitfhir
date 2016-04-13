e0f0be5d033c601f52bcaaeda81d5224f39c0c37
Jim Belcher <jbelcher@riis.com>
2016-04-13 14:50:20 -0400

    test

--- FitBit/TokenManager.php
+++ FitBit/TokenManager.php
@@ -89,7 +89,8 @@ class TokenManager {
         # Get the response
         $response = curl_exec($ch);
         curl_close($ch);
-var_dump($response);
+
+        var_dump($response);
         $objResponse = json_decode($response);
         
         $this->accessToken = $objResponse->access_token;
